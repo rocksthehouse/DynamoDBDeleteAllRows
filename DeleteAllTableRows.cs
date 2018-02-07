8805c8b7ba2ab4c0e1f149561d9c35acc2f5bd01
rocksthehouse <22393135+rocksthehouse@users.noreply.github.com>
2018-02-07 18:11:58 +0000

    Add files via upload
    Initial commit.

---
+++ DeleteAllTableRows.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+namespace DynamoDeleteAllRows
+{
+    /// <summary>
+    /// Class for deleting all rows from an AWS DynamoDB table.
+    /// </summary>
+    internal class DeleteAllTableRows
+    {
+        // This is fixed by AWS.
+        private const int maxBatchSize = 25;
+
+        private readonly List<WriteRequest> deleteRequestList = new List<WriteRequest>();
+
+        public void DeleteAllRows(string tableName, string hashKey, string rangeKey)
+        {
+            bool rangeKeyExists = !string.IsNullOrWhiteSpace(rangeKey);
+
+            using (var dbClient = new AmazonDynamoDBClient())
+            {
+                var scanRequest = new ScanRequest(tableName) { ProjectionExpression = rangeKeyExists ? $"{hashKey},{rangeKey}" : $"{hashKey}" };
+
+                ScanResponse scanResponse;     
+
+                while ((scanResponse = dbClient.Scan(scanRequest)).ScannedCount > 0)
+                {
+                    foreach (var item in scanResponse.Items)
+                    {
+                        var deleteKeys = new Dictionary<string, AttributeValue> { { hashKey, item[hashKey] } };
+
+                        if (rangeKeyExists)
+                        {
+                            deleteKeys.Add(rangeKey, item[rangeKey]);
+                        }
+
+                        if (deleteRequestList.Count >= maxBatchSize)
+                        {
+                            BatchDelete(dbClient, tableName, deleteRequestList);
+                        }
+
+                        deleteRequestList.Add(new WriteRequest(new DeleteRequest(deleteKeys)));
+                    }
+
+                    BatchDelete(dbClient, tableName, deleteRequestList);
+
+                    scanRequest.ExclusiveStartKey = scanResponse.LastEvaluatedKey;
+                }
+            }
+        }
+
+        private static void BatchDelete(IAmazonDynamoDB dbClient, string tableName, List<WriteRequest> deleteRequestList)
+        {
+            if (deleteRequestList.Any())
+            {
+                var itemsToDelete = new Dictionary<string, List<WriteRequest>> { { tableName, deleteRequestList } };
+
+                var batchDeleteRequest = new BatchWriteItemRequest
+                {
+                    RequestItems = itemsToDelete
+                };
+
+                dbClient.BatchWriteItem(batchDeleteRequest);
+            }
+
+            deleteRequestList.Clear();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DynamoDeleteAllRows
+{
+    /// <summary>
+    /// Class for launching the application.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point to the application.
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 2 || args.Length == 3)
+                {
+                    var tableName = args[0];
+                    var hashKey = args[1];
+
+                    string rangeKey = null;
+
+                    if (args.Length == 3)
+                    {
+                        rangeKey = args[2];
+                    }
+
+                    if (string.IsNullOrWhiteSpace(tableName) || string.IsNullOrWhiteSpace(hashKey) || args.Length == 3 && string.IsNullOrWhiteSpace(rangeKey))
+                    {
+                        DisplaySyntaxError();
+                    }
+                    else
+                    {
+                        new DeleteAllTableRows().DeleteAllRows(tableName, hashKey, rangeKey);
+                    }
+                }
+                else
+                {
+                    DisplaySyntaxError();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed due to exception: {ex}");
+            }
+        }
+
+        private static void DisplaySyntaxError()
+        {
+            Console.WriteLine("Expected syntax: DynamoDeleteAllRows.exe table_name hash_key_name [range_key_name]");
+            Console.WriteLine("AWS profile and region name can be specified by modifying the application configuration file.");
+        }
+    }
+}
